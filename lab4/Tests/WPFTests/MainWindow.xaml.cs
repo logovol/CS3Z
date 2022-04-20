09745f430c706f4a1ea983914a230ee01881f82a
logovol <50459467+logovol@users.noreply.github.com>
2022-04-20 22:07:26 +0300

    lab4

---
+++ lab4/Tests/WPFTests/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows;
+
+namespace WPFTests
+{
+    public partial class MainWindow
+    {
+        public MainWindow() => InitializeComponent();
+
+    }
+}
---
+++ lab4/Tests/WPFTests/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+using WPFTests.ViewModels.Base;
+
+namespace WPFTests.ViewModels
+{
+    class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Тестовое окно";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+            //set
+            //{
+            //    if (_Title == value) return;
+            //    _Title = value;
+            //    //OnPropertyChanged("Title");
+            //    //OnPropertyChanged(nameof(Title));
+            //    OnPropertyChanged();
+        }
+
+        private string _Email = "logovol@yandex.ru";
+
+        public string Email
+        {
+            get => _Email;
+            set => Set(ref _Email, value);
+        }
+
+        public DateTime CurrentTime => DateTime.Now;
+
+        private bool _TimerEnabled = true;
+
+        public bool TimerEnabled
+        {
+            get => _TimerEnabled;
+            set
+            {
+                if (!(Set(ref _TimerEnabled, value))) return;
+                _Timer.Enabled = value;
+            }
+        }
+
+        private readonly Timer _Timer;
+
+        public MainWindowViewModel()
+        {
+            _Timer = new Timer(100);
+            _Timer.Elapsed += OnTimerElapsed;
+            _Timer.AutoReset = true;
+            _Timer.Enabled = true;
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            OnPropertyChanged(nameof(CurrentTime));
+        }
+    }    
+}
