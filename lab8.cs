fcd1881f014edfea42677dd7c873363a18e3d57f
logovol <pasha1985@inbox.ru>
2022-05-06 14:50:55 +0300

    add query

--- lab8.cs
+++ lab8.cs
@@ -31,3 +31,17 @@ GROUP BY Name
 HAVING SUM(1) > 1;
 
 //4. 5
+
+//5.Запрос из столбца в строку по группам
+CREATE TABLE hometask (id int, group_id int, descr VARCHAR(10));
+INSERT INTO hometask VALUES (1, 1, 'Один'), (2, 2, 'Два'), (3, 1, 'Три'), (4, 2, 'Четыре'), (5, 2, 'Пять');
+SELECT * FROM hometask;
+GO
+
+SELECT group_id,
+       STUFF((SELECT ',' + CAST(descr AS VARCHAR)
+              FROM hometask t2
+              WHERE t1.group_id = t2.group_id FOR XML PATH('')),1,1,'') descr
+FROM hometask t1
+GROUP BY group_id;
+GO
